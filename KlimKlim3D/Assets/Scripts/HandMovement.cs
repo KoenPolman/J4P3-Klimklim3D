3f30e2562b08eac25b8a04ed71f947ba5a5e183f
Koen Polman <KoenPolman_@outlook.com>
2026-02-10 09:17:17 +0100

    feat: added code to handmovement

--- KlimKlim3D/Assets/Scripts/HandMovement.cs
+++ KlimKlim3D/Assets/Scripts/HandMovement.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HandMovement : MonoBehaviour
 {
+    [SerializeField] HandType handType;
+    [SerializeField] float maxRadius;
+
+    Transform shoulderPosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        shoulderPosition = transform.parent;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = new Vector3();
+        mousePos = Input.mousePosition;
+        Debug.Log(mousePos);
+        // if the mouse button bound to the hand in not down place the hand
+        if (!Input.GetMouseButtonDown((int)handType))
+        {
+            placeHand();
+        }
+    }
+    /// <summary>
+    /// place the hand at the mouse position
+    /// </summary>
+    void placeHand()
+    {
+
     }
 }
