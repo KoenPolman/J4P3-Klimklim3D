b3b8055224fe51f67fe52719bdf2cd6edabf3320
Koen Polman <KoenPolman_@outlook.com>
2026-02-18 13:57:02 +0100

    feat: added checking for holds
    also cut dead code from hand movement

--- KlimKlim3D/Assets/Scripts/HandMovement.cs
+++ KlimKlim3D/Assets/Scripts/HandMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class HandMovement : MonoBehaviour
 {
     [SerializeField] float maxRadius;
-    [SerializeField] private Camera mainCamera;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float moveSpeed = 10f;
 
@@ -17,10 +16,6 @@ public class HandMovement : MonoBehaviour
 
     private void Awake()
     {           
-        if (mainCamera == null)
-        {
-            mainCamera = Camera.main;
-        }
         shoulderPosition = transform.parent.transform.position;
         restingPosition = transform.parent.GetChild(1).position;
         limbInfo = GetComponent<LimbInfo>();
