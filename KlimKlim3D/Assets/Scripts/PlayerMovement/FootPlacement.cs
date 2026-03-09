05f2ded28aa284f82a9b51984f057729bd0bc480
Koen Polman <KoenPolman_@outlook.com>
2026-03-09 11:57:14 +0100

    Feat: feet move to hold nearest resting position when able

--- KlimKlim3D/Assets/Scripts/PlayerMovement/FootPlacement.cs
+++ KlimKlim3D/Assets/Scripts/PlayerMovement/FootPlacement.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class FootPlacement : MonoBehaviour
 {
-    private Hold[] holds;//references to holds
+    private Hold[] holds;// References to holds
     private Transform restingPosition;
     private HandMovement handMovement;
     private LimbInfo limbInfo;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        holds = FindObjectsByType<Hold>(FindObjectsSortMode.None);//get references to holds
+        holds = FindObjectsByType<Hold>(FindObjectsSortMode.None);// Get references to holds
         handMovement = GetComponent<HandMovement>();
         restingPosition = handMovement.GetRestingPosition();
         limbInfo = GetComponent<LimbInfo>();
@@ -19,7 +19,7 @@ public class FootPlacement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //check which hold is closest to the resting position of the foot
+        // Check which hold is closest to the resting position of the foot
         Hold targetHold = holds[0];
         for (int i = 0; i < holds.Length; i++)
         {
@@ -28,16 +28,18 @@ public class FootPlacement : MonoBehaviour
                 targetHold = holds[i];
             }
         }
-        //check if hold is available and within range
+        // Check if hold is available and within range
         if (targetHold.GetHoldAvailability(limbInfo.GetTypeStrict()) && Vector3.Distance(transform.parent.position, targetHold.transform.position) <= handMovement.GetMaxRadius())
         {
-            //update limb info which cascades into the foot actually moving to that hold
+            // Update limb info which cascades into the foot actually moving to that hold
             limbInfo.SetState(LimbState.holding);
+            // A hold reference has to be passed on to the hand/limb movement script, i am well aware that this is not the neatest solution so this might be changed in the future but for now please dont touch -Koen
             handMovement.SetHold(targetHold.transform);
+            // Notify the climbing manager that the foot is standing on a hold
         }
         else
         {
-            //if no hold is available go to the resting position
+            // If no hold is available go to the resting position
             limbInfo.SetState(LimbState.resting);
         }
     }
